5b90c40435a49e58bbf73210f34f115a6c3faf23
mfakca <mehmet.akca1@ogr.sakarya.edu.tr>
2021-06-09 18:57:01 +0300

    adding comment blocks

--- deneme.py
+++ deneme.py
@@ -18,34 +18,123 @@ class Text():
         
     
     def lower(self, text):
+        """
+        Converts the input to lowercase. (Girdiyi küçük harflere dönüştürür.)
+
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "MERHABA"
+        
+        Output:
+        
+        \ttext [string] => "merhaba"
+        """
         return text.translate(self.lower_table)
     
     def removePunc(self, text): 
+        """
+        Removes punctuation in the input. (Girdi içerisindeki noktalama işaretlerini atar.)
+
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "Daha mutlu olamam."
+        
+        Output:
+        
+        \ttext [string] => "Daha mutlu olamam"
+        """
+        
         return text.translate(str.maketrans('', '', string.punctuation))
     
-    # Hatalı
+    
     def sentTokenize(self, text):
-        # [^\.{2,}\!\?\:]*[\.{2,}\!\?\:]
+        """
+        Splits input into sentences. (Girdiyi cümlelere böler.)
+
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "Daha mutlu olamam. Bu akşam."
+        
+        Output:
+        
+        \tsentences [list <string>] => ["Daha mutlu olamam.", "Bu akşam."]
+        """
+       
         return re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s",text)
         
     def wordTokenize(self, text):
+        """
+        Splits input into words. (Girdiyi kelimelere böler.)
+
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "Daha mutlu olamam. Bu akşam."
+        
+        Output:
+        
+        \twords [list <string>] => ["Daha", "mutlu", "olamam", "Bu", "akşam"]
+        """
+        
         return self.removePunc(text).split()
     
         
     def sentAnalysis(self,text):
+        """
+        Calculates the emotion of the input using the trained model. It outputs label and score value. (Eğitilmiş model kullanılarak girdinin duygusunu hesaplar. Çıktı olarak duygu ve skor değerini verir.)
+
+        Trained model: https://huggingface.co/savasy/bert-base-turkish-sentiment-cased
+        
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "Daha mutlu olamam. Bu akşam."
+        
+        Output:
+        
+        \tlabel [string] => "negative" \n
+        \tscore [int] => 0.8
+        """
+        
         data = json.dumps({"inputs":text})
         response = requests.request("POST", self.SENT_ANALYSIS_API_URL, headers=self.sent_analysis_headers, data=data)
         result = json.loads(response.content.decode("utf-8"))[0][0]
+        
         return result["label"], result["score"]
         
 
     def generate(self,text):
+        """
+        Generates text based on input with a trained GPT-2 model. Returns the produced text as output. (Eğitilmiş GPT-2 modeliyle girdiye dayalı metin üretir. Çıktı olarak üretilmiş metni verir.)
+
+        Trained model: https://huggingface.co/adresgezgini/turkish-gpt-2
+        
+        Example: 
+        
+        Input:
+        
+        \ttext [string] => "Sakince arkana dön bir bak"
+        
+        Output:
+        
+        \tlabel [string] => "Sakince arkana dön bir bak ..." 
+        """
+        
         data = json.dumps({"inputs":text})
         response = requests.request("POST", self.GENERATE_API_URL, headers=self.generate_headers, data=data)
-        result = json.loads(response.content.decode("utf-8"))
-        return result[0]["generated_text"]
+        result = json.loads(response.content.decode("utf-8"))[0]
+        
+        return result["generated_text"]
 
    
     
 deneme = Text()
-print(deneme.generate("Merhaba, nasılsın. Bugün"))
+print(deneme.lower("Sakince arkana dön bir bak"))
