5689e3caf49a5f6ebb1a24c8ab08acb2dc22df70
mfakca <mehmet.akca1@ogr.sakarya.edu.tr>
2021-06-09 15:51:41 +0300

    adding sentiment analysis function

--- deneme.py
+++ deneme.py
@@ -1,6 +1,9 @@
 import numpy as np
 import re
 import string
+import json
+import requests
+
 
 
 class Text():
@@ -8,6 +11,8 @@ class Text():
         #self.text = text
         self.lower_table = str.maketrans("ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZWXQ","abcçdefgğhiıjklmnoöprsştuüvyzwxq")
         self.noktalama_isaretleri = np.array(["...",".","?","!",":"])
+        self.API_URL = "https://api-inference.huggingface.co/models/savasy/bert-base-turkish-sentiment-cased"
+        self.headers = {"Authorization": "Bearer api_gxjjPVmHDGvBlYfwPqRkzgecnMbPXjWAaY"}
     
     def lower(self, text):
         return text.translate(self.lower_table)
@@ -23,6 +28,18 @@ class Text():
     def wordTokenize(self, text):
         return self.removePunc(text).split()
     
+        
+    def sentAnalysis(self,text):
+        payload = {"inputs":text}
+        data = json.dumps(payload)
+        response = requests.request("POST", self.API_URL, headers=self.headers, data=data)
+        result = json.loads(response.content.decode("utf-8"))[0][0]
+        return result["label"], result["score"]
+        
+
+
+    
     
 deneme = Text()
-print(deneme.sentTokenize("Merhaba, nasılsın.Bugün"))
+label, score = deneme.sentAnalysis("Merhaba, nasılsın. Bugün")
+print(label)
